915adbc6b51700192f9d6dd343095c4527292987
Nadia-128bit <gomezperez.nadia@gmail.com>
2025-04-23 14:35:02 -0700

    se agrega modelo standing

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Standing(models.Model):
+    """
+    Model representing a League of Legends standing.
+    """
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+
