df6e04de9c043df7a013bd2a5d8dfb3215685618
Nadia-128bit <gomezperez.nadia@gmail.com>
2025-04-23 14:48:35 -0700

    se quita env

--- wiki/models.py
+++ wiki/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 class Standing(models.Model):
-    """
-    Model representing a League of Legends standing.
-    """
+   
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
 
